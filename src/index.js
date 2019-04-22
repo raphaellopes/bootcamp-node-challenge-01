31590c10f024d583f92fc07622964d1f2de4f097
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-22 17:26:51 -0300

    Add check post route and implements rule to redirects based on age

--- src/index.js
+++ src/index.js
@@ -12,10 +12,16 @@ nunjucks.configure('src/views', {
   watch: true,
 });
 
+app.use(express.urlencoded({ extended: false }));
+
 app.set('view engine', 'njk');
 
 // routes
 app.get('/', (req, res) => res.render('index'));
+app.post('/check', (req, res) => {
+  const { body: { age } } = req;
+  return res.redirect(`/${age >= 18 ? 'major' : 'minor'}?age=${age}`);
+});
 
 app.listen(PORT, () => (
   console.log(`Listening on port http://localhost:${PORT}`)));
