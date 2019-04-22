d817620625326e08ff1f527ceb7511e86c8be743
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-22 16:48:21 -0300

    Add nunjucks, configure it and add first view

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 // vendors
 const express = require('express');
+const nunjucks = require('nunjucks');
 
 const PORT = 3000;
 
 const app = express();
 
+nunjucks.configure('src/views', {
+  autoscape: true,
+  express: app,
+});
+
+app.set('view engine', 'njk');
+
 // routes
-app.get('/', (req, res) => res.send('Starting the project!'));
+app.get('/', (req, res) => res.render('index'));
 
 app.listen(PORT, () => (
   console.log(`Listening on port http://localhost:${PORT}`)));
