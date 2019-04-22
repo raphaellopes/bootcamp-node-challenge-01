0dcc2dda7ff64b9f523c88530f17ce7ea60f8650
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-22 15:55:55 -0300

    Add express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// vendors
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+
+// routes
+app.get('/', (req, res) => res.send('Starting the project!'));
+
+app.listen(PORT, () => (console.log(`Listening on port http://localhost:${PORT}`)));
