1cfd42d5904e310e0cd95ad3a0beaa0dfa4b5f3a
Raphael Lopes <ph.campanario07@gmail.com>
2019-04-22 17:49:10 -0300

    Create result template and implements logic to redirect on index.js

--- src/index.js
+++ src/index.js
@@ -16,12 +16,22 @@ app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'njk');
 
+const checkAge = age => (age >= 18 ? 'major' : 'minor');
+
 // routes
 app.get('/', (req, res) => res.render('index'));
 app.post('/check', (req, res) => {
   const { body: { age } } = req;
-  return res.redirect(`/${age >= 18 ? 'major' : 'minor'}?age=${age}`);
+  return res.redirect(`/${checkAge(age)}?age=${age}`);
 });
+
+app.get(
+  ['/major', '/minor'],
+  (req, res) => {
+    const { query: { age } } = req;
+    return res.render('result', { age, type: checkAge(age) });
+  },
+);
 
 app.listen(PORT, () => (
   console.log(`Listening on port http://localhost:${PORT}`)));
